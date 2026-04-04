2158d4b78a0ac3f87e91635f68b5fc3ade9863bf
lethinh0291 <lethinh0291@gmail.com>
2026-04-04 19:28:15 +0700

    update

---
+++ backend/BackendAPI/data/AppDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using BackendAPI.Models;
+
+namespace BackendAPI.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<POI> POIs { get; set; }
+}
+g
