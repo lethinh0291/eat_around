67ccc7f471e9b6eca1b9e2034a0895074f18ecd2
lethinh0291 <lethinh0291@gmail.com>
2026-04-16 20:41:57 +0700

    cap nhat giao dien

--- shared/SharedLib/obj/Debug/net10.0/SharedLib.AssemblyInfo.cs
+++ shared/SharedLib/obj/Debug/net10.0/SharedLib.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SharedLib")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33ecc7707e3570ac39ab7a8d5ab2693f4f5cfcef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+669fccc9e7b89c6cd2dca8193c748535acd8f36a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SharedLib")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SharedLib")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
